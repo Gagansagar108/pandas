4b7ec5caf21026494cc65116a3058bc2469f4af9
Gagansagar108 <gaganiitb108@gmail.com>
2021-08-25 18:32:52 +0530

    hola peter pandass.py

---
+++ pandass.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+df = pd.Series([1,2,3,4,54])
+df2=pd.DataFrame(np.random.randn(8,2))
+print(df2)
+df1 = pd.DataFrame(np.random.randn(6, 4),
+....: index=list('abcdef'),
+....: columns=list('ABCD')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
